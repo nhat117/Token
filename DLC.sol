bbdb8045ac56be55ae3348751682b881b2270236
Nhat Bui <72547907+nhat117@users.noreply.github.com>
2021-11-11 10:24:03 +0700

    Update DLC.sol

--- DLC.sol
+++ DLC.sol
@@ -23,9 +23,16 @@ contract DLCToken is BEP20Token{
     uint constant MONTH = 2629743;
     uint constant YEAR = 31556926;
     
-    uint256 constant DLCSUPPLY = 25000000;
+    /**
+     * @dev total coin supply and decimals
+     */
+
+    uint256 constant DLCSUPPLY = 2500000000;
     uint8 constant DECIMALS = 18;
-    //Initial participation project
+
+    /**
+     * @dev ultils for Transfer and Lock
+     */
     
     struct LockItem {
         uint256  releaseDate;
@@ -36,12 +43,13 @@ contract DLCToken is BEP20Token{
      * @dev add address of fund receiver for the initial fund allocation 
     */
     
-    address private marketingWallet= 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-    address private teamWallet = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
-    address private publicSaleWallet = 0x6ecaCced313Bc500aBE6E1ec34BE23888a8E777A;
-    address private privateSaleWallet = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
-    address private partnerWallet = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+    address private marketingWallet =   0x4fcD01Edf05b1EBD8f66638f6C9d0312Df8af4ce;
+    address private teamWallet =        0xB1C594206145e3401e4005A69114134c2E2a3fB3;
+    address private partnerWallet =     0x4E47FCf5908F5f88F33E7b0f558e234ABAA7C246;
+    address private publicSaleWallet =  0x6ecaCced313Bc500aBE6E1ec34BE23888a8E777A;
+    address private privateSaleWallet = 0xEde137b8baB8f4F15F26233c79b781EF8B11d538;
     
+
     mapping(address => uint256) private privateSale;
     mapping (address => LockItem[]) private lockList;
    
@@ -53,36 +61,53 @@ contract DLCToken is BEP20Token{
     uint256 amount;
     uint period;
     uint unit;
-    
+    uint256 amountWithDecimal = 10 ** uint256(DECIMALS);
     constructor() public payable BEP20Token("DLC","DLCTOKEN", DLCSUPPLY, DECIMALS) {
         
         //Private sale allocation
-        amount = 400000 * (10 ** uint256(DECIMALS));
-        BEP20Token.transfer(privateSaleWallet, amount);
+        
+        /**
+         * @dev modified the amount that want to transfer 
+         * @var amount enter a human friendly amount 
+         */
+
+       
+        amount = 40000000 
+        BEP20Token.transfer(privateSaleWallet, amount * amountWithDecimal);
         
         //Partner allocation
-        amount = 2500000 * (10 ** uint256(DECIMALS));
-        BEP20Token.transfer(partnerWallet, amount);
+        amount = 250000000 
+        BEP20Token.transfer(partnerWallet, amount * amountWithDecimal);
         
         //Team allocation
-        amount = 2500000 * (10 ** uint256(DECIMALS));
-        BEP20Token.transfer(teamWallet, amount);
+        amount = 250000000 
+        BEP20Token.transfer(teamWallet, amount * amountWithDecimal);
         
         //Marketing wallet allocation
-        amount = 250000 * (10 ** uint256(DECIMALS));
-        BEP20Token.transfer(marketingWallet, amount);
+        amount = 250000000 
+        BEP20Token.transfer(marketingWallet, amount * amountWithDecimal);
         
         //Timeline for Public allocation
-        period = 9;
+
+        /**
+         * @dev transfer amount according to the frequency period of a time unit
+         * @var period total period repeat overtime
+         * @var unit hour, month, year - could use these base constant unit declare at constant list to generate custom unit such as quarter, bi-annual,etc
+         * @var percentage sent certain % fund each time
+         * Total time period = period * unit
+         */
+
+        period = 20;
         unit =  3* MONTH;
+        uint8 percentage = 5;
         amount = BEP20Token.balanceOf(msg.sender); //Equivalance of 1 710 000 000 DLC
         for(uint i = 0; i < period; i ++) {
             quarterPublic.push(block.timestamp + i * unit);
         }
-        
         //Start publicSale
+    
         for(uint i = 0; i < quarterPublic.length; i ++) {
-            uint256 transferAmount = amount * 5 /100;
+            uint256 transferAmount = amount * uint(256) /100;
             amount.sub(transferAmount);
             transferAndLock(publicSaleWallet, transferAmount,   quarterPublic[i]);
         }
@@ -96,12 +121,13 @@ contract DLCToken is BEP20Token{
      * @param _amount The amount to be transferred.
      */
 
-	function transfer(address _receiver, uint256 _amount) public whenNotPaused returns (bool success) {
+    function transfer(address _receiver, uint256 _amount) public whenNotPaused returns (bool success) {
         require(_receiver != address(0)); 
         require(_amount <= getAvailableBalance(msg.sender));
         return BEP20Token.transfer(_receiver, _amount);
-	}
-	/**
+    }
+
+    /**
      * @dev transfer of token on behalf of the owner to another address. 
      * always require the owner has enough balance and the sender is allowed to transfer the given amount
      * @return the bool true if success. 
@@ -127,65 +153,120 @@ contract DLCToken is BEP20Token{
      * @param _amount The amount to be transferred.
      */
 
-	function transferAndLock(address _receiver, uint256 _amount, uint256 _releaseDate) public returns (bool success) {
-	    //Require the transferAndLock for only few wallet address
-	    require(msg.sender == teamWallet || msg.sender == privateSaleWallet || msg.sender ==   marketingWallet || msg.sender == owner() || msg.sender == partnerWallet);
+    function transferAndLock(address _receiver, uint256 _amount, uint256 _releaseDate) public returns (bool success) {
+        //Require the transferAndLock for only few wallet address
+        require(msg.sender == teamWallet || msg.sender == privateSaleWallet || msg.sender ==   marketingWallet || msg.sender == owner() || msg.sender == partnerWallet);
         BEP20Token._transfer(msg.sender,_receiver,_amount);
-    	
-    	if (lockList[_receiver].length==0) lockedAddressList.push(_receiver);
-		
-    	LockItem memory item = LockItem({amount:_amount, releaseDate:_releaseDate});
-		lockList[_receiver].push(item);
-	
+        
+        if (lockList[_receiver].length==0) lockedAddressList.push(_receiver);
+        
+        LockItem memory item = LockItem({amount:_amount, releaseDate:_releaseDate});
+        lockList[_receiver].push(item);
+    
         return true;
+    }
+   
+    // Dapp Functionality
+
+	/**
+     * @dev qurry the list of address that have at least a fund locked currently or in the past
+     * @return the list of all addresses 
+     */
+
+    function getLockedAddresses() public view returns (address[] memory) {
+	    return lockedAddressList;
 	}
 	
-    /**
-     * @return the total amount of locked funds of a given address.
-     * @param lockedAddress The address to check.
+	/**
+     * @dev count the number of account that have at least a fund locked currently or in the past
+     * @return the number of addresses 
      */
-     
-	function getLockedAmount(address lockedAddress) public view returns(uint256 _amount) {
-	    uint256 lockedAmount =0;
-	    for(uint256 j = 0; j<lockList[lockedAddress].length; j++) {
-	        if(now < lockList[lockedAddress][j].releaseDate) {
-	            uint256 temp = lockList[lockedAddress][j].amount;
-	            lockedAmount += temp;
+
+	function getNumberOfLockedAddresses() public view returns (uint256 _count) {
+	    return lockedAddressList.length;
+	}
+	    
+	    
+	/**
+     * @dev check the number address that have at least a fund locked currently
+     * @return the number of addresses 
+     */
+
+	function getNumberOfLockedAddressesCurrently() public view returns (uint256 _count) {
+	    uint256 count=0;
+	    for(uint256 i = 0; i<lockedAddressList.length; i++) {
+	        if (getLockedAmount(lockedAddressList[i])>0) count++;
+	    }
+	    return count;
+	}
+	    
+	/**
+     * @dev check the list of address that have at least a fund locked currently
+     * @return the list of all addresses 
+     */
+
+	function getLockedAddressesCurrently() public view returns (address[] memory) {
+	    address [] memory list = new address[](getNumberOfLockedAddressesCurrently());
+	    uint256 j = 0;
+	    for(uint256 i = 0; i<lockedAddressList.length; i++) {
+	        if (getLockedAmount(lockedAddressList[i])>0) {
+	            list[j] = lockedAddressList[i];
+	            j++;
 	        }
 	    }
-	    return lockedAmount;
-	}
-	
-	/**
-     * @return the total amount of locked funds at the current time
-     */
-     
-	function getLockedAmountTotal() public view returns(uint256 _amount) {
-	    uint256 sum =0;
-	    for(uint256 i = 0; i<lockedAddressList.length; i++) {
-	        uint256 lockedAmount = getLockedAmount(lockedAddressList[i]);
-    	    sum = sum.add(lockedAmount);
-	    }
-	    return sum;
-	}
-	
-	/**
-     * @return the total amount of locked funds of a given address.
+	    
+        return list;
+    }
+
+    /**
+     * @dev check locked funds of a given address.
+     * @return the total amount of locked fund in an address
      * @param lockedAddress The address to check.
      */
      
-	function getAvailableBalance(address lockedAddress) public view returns(uint256 _amount) {
-	    uint256 bal = BEP20Token.balanceOf(lockedAddress);
-	    uint256 locked = getLockedAmount(lockedAddress);
-	    return bal.sub(locked);
-	}
-	
-	/**
-	 * @return the total amount of circulating coins that are not locked at the current time
-	 * 
-	 */
-	 
-	function getCirculatingSupplyTotal() public view returns(uint256 _amount) {
-	    return BEP20Token.totalSupply().sub(getLockedAmountTotal());
-	}
+    function getLockedAmount(address lockedAddress) public view returns(uint256 _amount) {
+        uint256 lockedAmount =0;
+        for(uint256 j = 0; j<lockList[lockedAddress].length; j++) {
+            if(block.timestamp < lockList[lockedAddress][j].releaseDate) {
+                uint256 temp = lockList[lockedAddress][j].amount;
+                lockedAmount += temp;
+            }
+        }
+        return lockedAmount;
+    }
+    
+    /**
+     * @dev  check the total of locked funds at the current time
+     * @return the total amount of locked fund
+     */
+     
+    function getLockedAmountTotal() public view returns(uint256 _amount) {
+        uint256 sum =0;
+        for(uint256 i = 0; i<lockedAddressList.length; i++) {
+            uint256 lockedAmount = getLockedAmount(lockedAddressList[i]);
+            sum = sum.add(lockedAmount);
+        }
+        return sum;
+    }
+    
+    /**
+     * @dev check of locked funds of a given address.
+     * @return the total amount 
+     * @param lockedAddress The address to check.
+     */
+     
+    function getAvailableBalance(address lockedAddress) public view returns(uint256 _amount) {
+        uint256 bal = BEP20Token.balanceOf(lockedAddress);
+        uint256 locked = getLockedAmount(lockedAddress);
+        return bal.sub(locked);
+    }
+    
+    /**
+     * @dev  check amount circulating coins that are not locked at the current time
+     * @return the total amount
+     */
+     
+    function getCirculatingSupplyTotal() public view returns(uint256 _amount) {
+        return BEP20Token.totalSupply().sub(getLockedAmountTotal());
+    }
 }
